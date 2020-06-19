f5c2a37642a2c1046d80c67b5bc6427ee54cfa44
Dasylum <ds19987@gmail.com>
2020-06-19 11:36:27 +0530

    Add files via upload

--- auth/auth.js
+++ auth/auth.js
@@ -1,24 +1,38 @@
 var jwt = require('jsonwebtoken');
+var express = require('express');
+var app = express();
+var cookieParser = require('cookie-parser');
+app.use(cookieParser());
+
+
 
 const authenticateJWT = (req, res, next) => {
-  const authHeader = req.headers.authorization;
+  
+  const { headers: { authorization } } = req;
 
   let accessTokenSecret = 'secret';
 
-  if(authHeader) {
-    const token = authHeader.split(' ')[1];
+  //var token = req.cookies.myCookie;
 
+   //console.log(token);
+
+  if(authorization){
+
+    const token = authorization.split(' ')[1];
+    
     jwt.verify(token, accessTokenSecret, (err, user) => {
+
        if (err) {
                 return res.sendStatus(403);
             }
             req.user = user;
+
             next();
-    }) 
+    })
   }
   else {
-    res.sendStatus(401);
+    next();
   }
-};
+}; 
 
 module.exports = authenticateJWT;
